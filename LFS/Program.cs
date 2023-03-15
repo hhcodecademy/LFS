6c65ab70a3d82a8a7278728c614da7c40bba6856
huseyn.hasanli <huseyn.hasanli@CAPR0>
2023-03-15 20:43:50 +0400

    Add project files.

---
+++ LFS/Program.cs
@@ -0,0 +1,23 @@
+﻿
+bool isContinue = true;
+string students = "";
+do
+{
+    Console.WriteLine("Dersde olan telebenin adini giriniz");
+    string fullName = Console.ReadLine();
+    students=students+fullName+"\n";
+    Console.WriteLine("Derse basqa telebe varmi? (h/y)");
+    char result = char.Parse(Console.ReadLine());
+    if (result == 'h' || result == 'H')
+    {
+        isContinue = true;
+    }
+    else {
+        isContinue = false;
+    }
+
+
+
+} while (isContinue);
+Console.WriteLine("Dersde olan telebler");
+Console.WriteLine(students);
